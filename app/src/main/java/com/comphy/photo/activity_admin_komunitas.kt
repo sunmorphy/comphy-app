f6fa28e824d5924df9ebf65d7cdec2497cd128f3
reza <rezafinola@gmail.com>
2022-05-23 17:56:52 +0700

    feature/create_community

---
+++ app/src/main/java/com/comphy/photo/activity_admin_komunitas.kt
@@ -0,0 +1,11 @@
+package com.comphy.photo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_admin_komunitas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_community_admin)
+    }
+}
